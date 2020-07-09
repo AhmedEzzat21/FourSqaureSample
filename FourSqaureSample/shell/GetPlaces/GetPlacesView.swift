d691505cb9407f7d7807fa3b412086f678706879
ahmed.ezzat <a7med.ibn.3zzat@gmail.com>
2020-07-09 12:54:10 +0200

    add navigation bar

--- FourSqaureSample/shell/GetPlaces/GetPlacesView.swift
+++ FourSqaureSample/shell/GetPlaces/GetPlacesView.swift
@@ -17,100 +17,111 @@ class GetPlacesView: BaseView<GetPlacesPresenter, BaseItem> ,CLLocationManagerDe
         didSet {
             self.placesCollection.register(UINib(nibName: String(describing: PlacesCollectionViewCell.self), bundle: nil), forCellWithReuseIdentifier: String(describing: PlacesCollectionViewCell.self))
             
-             self.placesCollection.register(UINib(nibName: String(describing: NoDataFoundCollectionViewCell.self), bundle: nil), forCellWithReuseIdentifier: String(describing: NoDataFoundCollectionViewCell.self))
+            self.placesCollection.register(UINib(nibName: String(describing: NoDataFoundCollectionViewCell.self), bundle: nil), forCellWithReuseIdentifier: String(describing: NoDataFoundCollectionViewCell.self))
             //NoDataFoundCollectionViewCell
         }
     }
     
     override func bindind() {
         presenter = GetPlacesPresenter(router: RouterManager(self), userRepo: UserRepoImpl())
-
-            self.locationManager.requestAlwaysAuthorization()
-            
-            // For use in foreground
-            self.locationManager.requestWhenInUseAuthorization()
-            
-            if CLLocationManager.locationServicesEnabled() {
-                locationManager.delegate = self
-                locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-                locationManager.startUpdatingLocation()
-                
-                locationManager.distanceFilter = 500 // distance changes you want to be informed about (in meters)
-                locationManager.desiredAccuracy = 100 // biggest approximation you tolerate (in meters)
-                locationManager.activityType = .automotiveNavigation // .automotiveNavigation will stop the updates when the device is not moving
-
-
-            }
-       
-       self.placesCollection.reloadData()
-
-      
-    }
-  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-      
-      
-      guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
-      self.currentLocation = locValue
-      
-      if firstLanche{
-          updatLocation()
-      }
-      
-      
-      
-  }
-    func updatLocation(){
-           
-           
         
-            presenter.getPlaces(lat: currentLocation!.latitude, lng: currentLocation!.longitude)
-          presenter.places.bind { _ in
-                             self.placesCollection.reloadData()
-                         }
-           firstLanche = false
-           
-           
-           
-           
-       }
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+            
+            locationManager.distanceFilter = 500 // distance changes you want to be informed about (in meters)
+            locationManager.desiredAccuracy = 100 // biggest approximation you tolerate (in meters)
+            locationManager.activityType = .automotiveNavigation // .automotiveNavigation will stop the updates when the device is not moving
+            
+            
+        }
+        
+        self.placesCollection.reloadData()
+        
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController!.isNavigationBarHidden = false
+        
+        let rightBtn : UIBarButtonItem = UIBarButtonItem(title: "Real time", style: .plain, target: self, action: #selector(onClickMethod))
+        
+        self.navigationItem.rightBarButtonItem = rightBtn
+        
+    }
+    @objc func onClickMethod() {
+        print("right bar button item")
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        self.currentLocation = locValue
+        
+        if firstLanche{
+            updatLocation()
+        }
+        
+        
+        
+    }
+    func updatLocation(){
+        
+        
+        
+        presenter.getPlaces(lat: currentLocation!.latitude, lng: currentLocation!.longitude)
+        presenter.places.bind { _ in
+            self.placesCollection.reloadData()
+        }
+        firstLanche = false
+        
+        
+        
+        
+    }
     
 }
 extension GetPlacesView: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if presenter.places.value.isEmpty {
-              return 1
+            return 1
         }
         else{
             
-           return presenter.places.value.count
+            return presenter.places.value.count
         }
-      
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if presenter.places.value.isEmpty {
             let cell = (collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: NoDataFoundCollectionViewCell.self), for: indexPath))  as! NoDataFoundCollectionViewCell
-                       
-              return cell
+            
+            return cell
         }else {
             let cell = (collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: PlacesCollectionViewCell.self), for: indexPath))  as! PlacesCollectionViewCell
             cell.configure(result: presenter.places.value[indexPath.row], index: indexPath.row)
-              return cell
+            return cell
         }
         
-            
+        
     }
     
- 
+    
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         if  presenter.places.value.isEmpty {
-             return CGSize( width: (collectionView.frame.width), height:collectionView.frame.height )
+            return CGSize( width: (collectionView.frame.width), height:collectionView.frame.height )
         }
         else {
-             return CGSize( width: (collectionView.frame.width), height: 120 )
+            return CGSize( width: (collectionView.frame.width), height: 120 )
         }
-       
+        
     }
     
 }
